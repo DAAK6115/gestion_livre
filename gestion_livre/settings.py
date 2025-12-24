25df19ec8be1e6031e6c374a2d32f2f7bb556d85
DAAK6115 <122787773+DAAK6115@users.noreply.github.com>
2025-12-24 00:26:46 +0000

    Ajout d'utilisateur V2

--- gestion_livre/settings.py
+++ gestion_livre/settings.py
@@ -102,8 +102,8 @@ WSGI_APPLICATION = 'gestion_livre.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
     )
 }
@@ -153,8 +153,9 @@ if SUPERUSERS:
             if not User.objects.filter(username=username).exists():
                 User.objects.create_superuser(
                     username=username,
+                    email=email,
                     password=password
-                )
+                    )
                 print(f"Superuser {username} created")
         except Exception as e:
             print("Error creating superuser:", e)
